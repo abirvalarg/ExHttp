03867d2b2a31ba78fe4a70ea673cd1bed789aa43
abirvalarg <yurnik2004@gmail.com>
2022-09-21 13:34:11 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExHttp.MixProject do
   def project do
     [
       app: :ex_http,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -14,7 +14,7 @@ defmodule ExHttp.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      # extra_applications: [:logger]
     ]
   end
 
